e68e1292e68e667307f1fce401538142ff18945b
Bryan Houlton <brhoulton@gmail.com>
2020-09-15 22:50:02 -0400

    working

--- pystocket/pystocket.py
+++ pystocket/pystocket.py
@@ -4,21 +4,21 @@ import pandas as pd
 url = "https://rtstockdata.azurewebsites.net/request"
 headers = {'Content-Type': "application/json", 'Accept': "application/json"}
 
-class TokenError(Execption):
+class TokenError(Exception):
     pass
 
 def get(ticker, start, end, token = "", pandas = False):
-    response = requests.get(url, headers = headers, json = {'ticker': ticker, 'start': start, 'end': end, 'token': token}).json()
+    response = requests.get(url, headers = headers, json = {'ticker': ticker, 'start': start, 'end': end, 'token': token})
     
-    if response['message'] == 'bad chars':
+    if response.json()['message'] == 'bad chars':
         raise ValueError("Please exclude quotation characters and semicolons from your queries. This helps protect us from SQL injection attacks.")
     
-    if response['message'] == 'Invalid Token':
+    if response.json()['message'] == 'Invalid Token':
         raise TokenError("Invalid API Token")
 
     parsed = {}
     for pair in response.json()['data']['recordsets'][0]:
-        parsed.update({pair['datetime']: pair['price']})
+        parsed.update({pair['dt']: pair['price']})
 
     if pandas:
         parsed = pd.Series(parsed, name = 'price')
@@ -28,5 +28,5 @@ def get(ticker, start, end, token = "", pandas = False):
 
     return parsed
 
-data = get('MSFT', "", "", pandas=True)
+data = get('MSFT', "09/15/2020 22:31", "09/15/2020 22:34")
 print(data)
