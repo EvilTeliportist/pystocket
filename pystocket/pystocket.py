9049bdb75fec4cdda5fc61259a1623b9b8147818
Bryan Houlton <brhoulton@gmail.com>
2020-09-17 20:12:03 -0400

    package changes

--- pystocket/pystocket.py
+++ pystocket/pystocket.py
@@ -4,7 +4,7 @@ import yahoo_fin as yf
 import yahoo_fin.stock_info as si
 import yahoo_fin.options as op
 import matplotlib.pyplot as plt
-import seaborn as sns
+
 url = "https://rtstockdata.azurewebsites.net/request"
 headers = {'Content-Type': "application/json", 'Accept': "application/json"}
 
