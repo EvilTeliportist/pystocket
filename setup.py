9040081ffef5fbef0869d2c26e813638b547fa7a
Bryan Houlton <brhoulton@gmail.com>
2020-09-17 20:19:34 -0400

    package changes

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from distutils.core import setup
 setup(
   name = 'pystocket',         # How you named your package folder (MyLib)
   packages = ['pystocket'],   # Chose the same as "name"
-  version = '1.2',      # Start with a small number and increase it with every change you make
+  version = '1.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Stocket API Connection Package',   # Give a short description about your library
   author = 'EvilTeliportist',                   # Type in your name
   author_email = 'stocketapi@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/EvilTeliportist/pystocket/',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/EvilTeliportist/pystocket/archive/1.2.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/EvilTeliportist/pystocket/archive/1.3.tar.gz',    # I explain this later on
   keywords = [],   # Keywords that define your package best
-  install_requires=['requests', 'pandas', 'numpy'],
+  install_requires=['requests', 'pandas', 'numpy', 'yahoo_fin', 'matplotlib'],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
